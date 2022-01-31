7cc8c435b602450340b4d87d6783811e38ad7ef1
fadouaabdoul <fadwaabdoul@gmail.com>
2022-01-31 01:01:10 +0100

    checking branch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import html from "./index.html";
 import {Seule} from 'seule'
-//where you import your component 
+//where you import your component
 const app = new Seule({
     el: '#app',
     style: 'app',
