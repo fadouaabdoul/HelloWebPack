87b77b9c6d3db33bdbee0cac331147f651922b47
fadouaabdoul <fadwaabdoul@gmail.com>
2022-02-01 20:19:34 +0100

    getting start with seuleJs

---
+++ src/js_files/header.js
@@ -0,0 +1,14 @@
+import {Seule} from 'seule'
+import header_page from "../page/header_page.html"
+import header_style from "../css/header.css"
+
+//where you import your component
+const e = new Seule({
+    el: '#e',
+    style: 'header_style',
+    tag: 'section',
+    handler: {
+
+    }
+
+})
